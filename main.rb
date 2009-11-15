2d54295a45c91b206d5898129f74dc2c9c83ef78
Eric Lavigne <lavigne.eric@gmail.com>
2009-11-15 14:42:36 -0500

    Added another simple route. Removed some logging code that wasn't working. Default logging is good enough.

--- main.rb
+++ main.rb
@@ -2,28 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-def log_message(msg)
-	e = request.env['sinatra.error']
-	puts msg.to_s
-end
-
-def log_error(msg,err)
-	puts msg.to_s
-	puts err.backtrace.join("\r\n")
-end
-
-error do
-	e = request.env['sinatra.error']
-	log_error("Unhandled error",request.env['sinatra.error'])
-	"Application error"
-end
-
 get '/' do
   haml :index
 end
 
+get '/hello/:world' do |world|
+	"Hello #{world}"
+end
+
 get '/divide_by_zero' do
-	log_message('About to divide by 0')
 	7 / 0
 end
 
